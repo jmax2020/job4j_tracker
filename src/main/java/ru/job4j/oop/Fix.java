fd9e982f6d9b349cdafd008db72c11ebb746d736
jmax2020 <jmax2020@mail.ru>
2021-01-15 00:33:31 +0700

    1.7. Конструктор [#427998]

---
+++ src/main/java/ru/job4j/oop/Fix.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+public class Fix {
+
+    private String desk;
+
+    public Fix(String text) {
+        desk = text;
+    }
+
+    public static void main(String[] args) {
+       Fix bug = new Fix("Bug");
+    }
+
+}
