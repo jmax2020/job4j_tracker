2a50ccb07a24c28577e3e9c3da29e144e39242e0
jmax2020 <jmax2020@mail.ru>
2021-01-21 01:20:39 +0700

    4. Переопределение [#428004]

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,15 @@
+package ru.job4j.inheritance;
+
+import java.sql.SQLOutput;
+
+public class JSONReport extends TextReport{
+    public String generate(String name, String body) {
+        return '{' + System.lineSeparator() + " \"name\" : \"" + name + "\"" + System.lineSeparator() + " \"body\" : \"" + body + "\"" + System.lineSeparator() + '}';
+    }
+
+    public static void main(String[] args) {
+        JSONReport json = new JSONReport();
+        String text = json.generate("max", "factor");
+        System.out.println(text);
+    }
+}
