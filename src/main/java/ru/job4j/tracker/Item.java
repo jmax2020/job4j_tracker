72bd5c76053051f6256de7ec929cfc5a623eb187
jmax2020 <jmax2020@mail.ru>
2021-01-27 01:05:55 +0700

    7. Date. Отображение даты. [#428007]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,24 +1,31 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created;
 
     public Item() {
     }
 
     public Item(int id) {
         this.id = id;
+        this.created = LocalDateTime.now();
     }
 
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+        this.created = LocalDateTime.now();
     }
 
     public int getId() {
         return id;
     }
+
+    public LocalDateTime getCreated() { return this.created; }
 
     public void setId(int id) {
         this.id = id;
