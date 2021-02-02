f89f4c365562e5790690f0d7d6fdeb8c4b7a5fb1
jmax2020 <jmax2020@mail.ru>
2021-02-02 23:56:59 +0700

    6. Метод замены заявки. Tracker.replace [#428015]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,15 +14,8 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findAll(Item item) {
@@ -40,5 +33,27 @@ public class Tracker {
         }
         massName = Arrays.copyOf(massName, size);
         return massName;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for(int i = 0; i < size; i++) {
+            if(items[i].getId() == id) {
+                rsl = i;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        item.setId(id);
+        if(index != -1) {
+            items[index] = item;
+            return true;
+        } else {
+            return false;
+        }
     }
 }
