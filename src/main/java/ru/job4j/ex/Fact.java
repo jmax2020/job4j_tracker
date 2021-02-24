1378994ead0f432307efc7ab5d47cfb627bde574
jmax2020 <jmax2020@mail.ru>
2021-02-25 00:06:48 +0700

    0.2. Кидаем исключение - throw new RuntimeException [#427960]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,18 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+        System.out.println(new Fact().calc(-5));
+    }
+
+    public int calc(int n) {
+        if(n < 0) {
+            throw new IllegalArgumentException("Аргумент меньше нуля! Вычисление фаткориала остановлено.");
+        }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
