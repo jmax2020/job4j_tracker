c670847965ac2b1814680075fe29ae8e5cb28c3e
jmax2020 <jmax2020@mail.ru>
2021-03-11 23:21:02 +0700

    test

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -16,6 +16,32 @@ public class FindEl {
         return rsl;
     }
 
+    public static boolean sent(String value, String[] abuses) throws ElementAbuseException {
+        for (String element : abuses) {
+            if(value.equals(element)) {
+               throw new ElementAbuseException("The element is in abuse!");
+            }
+        }
+        return true;
+    }
+
+    public static void process(String[] values, String key, String[] abuses) {
+        try {
+            if(indexOf(values, key) != -1) {
+                sent(key, abuses);
+            }
+        } catch (ElementAbuseException ea) {
+            ea.printStackTrace();
+        } catch (ElementNotFoundException en) {
+            en.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } catch (Throwable th) {
+            th.printStackTrace();
+        }
+
+    }
+
     public static void main(String[] args) {
         String[] mass = {"one", "two", "three"};
         try {
